150f4de39ae2abc07c79315432cba49c538a29f7
John Telfeyan <john.telfeyan@gmail.com>
2019-01-05 15:20:07 -0500

    made functions more pythonic, improved docstrings

--- extract-transform-load/pandas_postgres/PandagreSQL.py
+++ extract-transform-load/pandas_postgres/PandagreSQL.py
@@ -9,7 +9,7 @@ from configparser import ConfigParser
 from sqlalchemy import create_engine
 
 class PandagreSQL:
-    """Synopsis: A frankenstien combination of functions for ETLing pandas DFs to and from PostgreSQL
+    """Synopsis: Additinal functions for ETLing pandas DFs to and from PostgreSQL to help novice data scientists
     
     Usage: Best practice: read in connection info from a .ini config file, the paramaters can 
     be over written and specified ad hoc as kwargs
@@ -24,8 +24,17 @@ class PandagreSQL:
              
     """
     def __init__(self, **kwargs):
-        """ example use: db = PandagreSQL(config_filename="secrets/config.ini", meta_section="default")
+        """ 
+        Example 1, Connect with ini file:
+        db = PandagreSQL(config_filename="secrets/config.ini", meta_section="default")
         include your .ini in a .gitignore 
+        
+        Example 2, Connect to local DB with default settings :
+        db = PandagreSQL()                    #(no password)
+        db = PandagreSQL(password="Pa$$w0rd") #(custom password)
+        
+        Example 3, Build, but dont connect:
+        db = PandagreSQL(connect=False)
         
         """
         self.credentials = { "user" : "postgres",
@@ -38,14 +47,17 @@ class PandagreSQL:
         self.connected = False
         self.meta = {}
         
+        # First load files from config file (Best practice)
         if "config_filename" in kwargs and "meta_section" in kwargs:
             self.get_connection_from_ini(config_filename=kwargs.get("config_filename"),
                                          meta_section=kwargs.get("meta_section"))
         
+        # Check if the user overided config credentials in the declaration
         for key in self.credentials:
             if key in kwargs:
                 self.credentials[key] = kwargs.get(key)
-                
+        
+        # Attempt to connect 
         connect_on_init = True if "connect" not in kwargs else kwargs.get("connect")
         self.build_connection_string(connect=connect_on_init)
         
@@ -53,8 +65,14 @@ class PandagreSQL:
             self.load_metadata()
     
     def can_sql(self):
+        """Build a dictionary of canned SQL statements specific to Postgres with short
+        descritpions:
+           "User Tables"   : df with all user created tables (excluding default PG tables)
+           "All Tables"    : df including user and PG administrative tables; editing these will break the DB
+           "All Databases" : df with list of all DBs accessable to user
+        """
         self.SQL = {}   #Dict of canned SQL 
-        self.SQL["All Non-admin Tables"] = '''
+        self.SQL["User Tabls"] = '''
         SELECT table_schema,table_name
         FROM information_schema.tables
         WHERE table_type = 'BASE TABLE' 
@@ -62,7 +80,7 @@ class PandagreSQL:
         ORDER BY table_schema,table_name;
         '''
         
-        self.SQL["All Tables and Admin Tables"] = '''
+        self.SQL["All Tables"] = '''
         SELECT table_schema,table_name
         FROM information_schema.tables
         ORDER BY table_schema,table_name;
@@ -76,19 +94,19 @@ class PandagreSQL:
     def build_connection_string(self, connect=True, **kwargs):
         ''' By default, uses the credtentials dictionary to build a connection string. Any
         credential can be overwritten by passing it as an argument.
-        
         '''
-        _user = self.credentials["user"] if kwargs.get('user') == None else kwargs.get('user')
-        _password = self.credentials["password"] if kwargs.get('password') == None else kwargs.get('password')
-        _host = self.credentials["host"] if kwargs.get('host') == None else kwargs.get('host')
-        _port = self.credentials["port"] if kwargs.get('port') == None else kwargs.get('port')
-        _name = self.credentials["name"] if kwargs.get('name') == None else kwargs.get('name')
-        delim = ":" if len(_password) >= 1 else ""
-        self.connection_string = 'postgresql://%s%s%s@%s:%s/%s' % (_user, delim, _password, _host,
-                                                                  _port, _name)
+        # Allow replacement of specific credentials, ad-hoc (e.g. user/pw)
+        _creds = {}
+        for key in self.credentials:
+            _creds[key] = self.credentials[key] if kwargs.get(key) == None else kwargs.get(key)
+        _delim = ":" if len(_creds["password"]) >= 1 else ""
+        _params = (_creds["user"], _delim, _creds["password"], _creds["host"], _creds["port"], _creds["name"])
+        self.connection_string = 'postgresql://%s%s%s@%s:%s/%s' % _params
         if connect: self.connect()
         
     def connect(self):
+        """ Connect using the self.connection string
+        """
         self.engine = create_engine(self.connection_string)
         self.conn = self.engine.connect()
         self.connected = True
@@ -129,7 +147,7 @@ class PandagreSQL:
             raise Exception('Section {0} not found in the {1} file'.format(meta_section, config_filename))
             
     def load_metadata(self):
-        """ query/requery your db connections metadata via postgres specfic SQL
+        """Query/requery your db connections metadata via postgres specfic SQL
         """
         self.meta["user_tables"] = pd.read_sql(self.SQL["All Non-admin Tables"], self.engine)
         self.meta["all_tables"] = pd.read_sql(self.SQL["All Tables and Admin Tables"], self.engine)
@@ -138,7 +156,7 @@ class PandagreSQL:
 
     
     def all_excel_worksheets_to_tables(self, excel_filename, schema_name="public", exists="fail"):
-        """
+        """Convert all worksheets in an Excel workbook to tables; sheet names become table names
         """
         xls=pd.ExcelFile(excel_filename)
         for sheet in xls.sheet_names:
@@ -151,25 +169,25 @@ class PandagreSQL:
         
 
             
-            
+"""            
 if __name__ == '__main__':
     # Example usage
-    # best practice
+    # best practice: connection info in config.ini
     db = PandagreSQL(config_filename="secrets/config.ini", meta_section="default")
     db.load_metadata()
     meta = db.meta #for spyder ide
     db.all_excel_worksheets_to_tables("data/data_type_test.xlsx", exists="append")
     
-    #one table
+    # add one table
     df = pd.read_excel("data/float_test.xlsx", "Sheet3")
     df.to_sql("float_test", db.engine, if_exists="replace")
-    db.load_metadata()
+    db.load_metadata() #check to see if it added
     
-    #override the ini config file and log in as read_only user:
-    rddb = db = PandagreSQL(config_filename="secrets/config.ini", 
+    # override the ini config file and log in as read_only user:
+    rddb = PandagreSQL(config_filename="secrets/config.ini", 
                             meta_section="default", user="read_only")
     
-
+"""
     
     
         
